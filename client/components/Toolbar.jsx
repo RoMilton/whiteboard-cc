a2d0d2ed1a3c499e19a3029c92ae26fcace24c5d
Rohan Milton <rohan.milton@sibdocity.com>
2016-09-29 16:11:32 +0100

    Styled mode buttons

--- client/components/Toolbar.jsx
+++ client/components/Toolbar.jsx
@@ -11,12 +11,23 @@ export default class Toolbar extends React.Component {
 	render(){
 		return (
 			<header className="toolbar">
+				<div className="toolbar__controls">
+					<div class="toolbar__button toolbar__button--filled"></div>
+					<div class="toolbar__button toolbar-button--undo">Undo</div>
+					<div class="toolbar__button toolbar-button--clear">Undo All
+					</div>
+				</div>
 				<ul className="mode-list">
-					<li className="mode-list__item mode-list__item--pen"></li>
+					<li className="mode-list__item mode-list__item--pen is-active"></li>
 					<li className="mode-list__item mode-list__item--line"></li>
 					<li className="mode-list__item mode-list__item--rect"></li>
 					<li className="mode-list__item mode-list__item--text"></li>
 				</ul>
+				<div className="toolbar__share">
+					<div className="toolbar__item toolbar__item--url">Change URL</div>
+					<div className="toolbar__item toolbar__item--name">Rohan</div>
+					<div className="toolbar__item-full toolbar__item--share">Invite/Share</div>
+				</div>
 			</header>
 		)
 	}
